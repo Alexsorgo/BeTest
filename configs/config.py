8e452e941cbd78985541ef31d687151fe953428d
sorgo <alexey.sorgo@gmail.com>
2018-09-03 15:49:18 +0300

    user_story: add config

---
+++ configs/config.py
@@ -0,0 +1,44 @@
+PASSWORD = '272b9acb0aaf1902c8b826de040ffabdf7cda8ca3f8355b0d117075c88a1c0d3ce7b3437718d1cbf54b5e5a996b1d6d749964671da49c9b20631ef7dc592a19c25369c6dbcccdc1a33944e18687eb801f74ff4775893edd5368f0037674feb70'
+MY_NUMBER = '8613777322455' # china
+# my_number = '12566018988' # america
+# server = '34.217.41.22'  # load
+SERVER = '54.201.154.141' # dev
+# server = '52.42.76.167' # pre-prod
+# server = '35.156.90.43' # prod
+# server = '192.168.0.103'
+
+
+# USERS:
+# User for test: main actions
+CHINA_NUMBER = "8613777322455"
+CHINA_FIRSTNAME = "China"
+CHINA_LASTNAME = "Autotest"
+CHINA_USERNAME = "CH"
+
+# User for test: friend request by phone number, p2p chat and group chat
+AMERICA_NUMBER = '12566018988'
+AMERICA_FIRSTNAME = 'America'
+AMERICA_LASTNAME = 'Autotest'
+AMERICA_USERNAME = 'US'
+
+# User for test: friend request by username
+PERU_NUMBER = '51997259024'
+PERU_FIRSTNAME = 'Peru'
+PERU_LASTNAME = 'Autotest'
+PERU_USERNAME = 'PE'
+
+# User for test: friend request by contacts
+UKRAINE_NUMBER = '380998681837'
+UKRAINE_FIRSTNAME = 'Ukraine'
+UKRAINE_LASTNAME = 'Autotest'
+UKRAINE_USERNAME = 'UA'
+
+
+NOTALLOWED_LOGIN = '2568386768'
+INCORRECT_LOGIN = '1111111111'
+FAKE_SMS = '903182'
+INCORRECT_FIRSTNAME = 'QWERTYUIOPASDFGHJKLZXCVBNMQWERTYUI'
+DELETE_MESSAGE = 'Deleted'
+STARED_MESSAGE = 'Star'
+GROUP_NAME = 'Fly'
+MAX_GROUP_NAME = 'qwertyuiopasdfghjklzxcvbnmqwertyuio'
