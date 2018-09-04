64b370367058c53bdc30e1548f1eef1dbfe63aa5
sorgo <alexey.sorgo@gmail.com>
2018-09-04 12:36:13 +0300

    user_story: Rename, update

--- models/search_by_username.py
+++ models/search_by_username.py
@@ -1,17 +1,18 @@
 import bert
 
 from erlastic import Atom
+from utils.logs import log
 
 
-def search_by_username():
+def search_by_username(user_id, search_username):
     module = Atom('Search')
-    user_id = 123123                     # id     = [] :: [] | integer(),
+    user_id = user_id                     # id     = [] :: [] | integer(),
     ref = 'username'                       # ref    = [] :: [] | binary(),
     field = 'nick'                     # field  = [] :: [] | binary(),
     type_r = Atom('==')                 # type   = [] :: [] | '==' | '!=' | 'like',
-    value = ['UA']             # value  = [] :: [] | term(),
+    value = [search_username]             # value  = [] :: [] | term(),
     status = Atom('contact')            # status = [] :: [] | profile | roster | contact | member | room
     by_phone_f = (module,user_id,ref,field,type_r,value,status)
     by_phone = bert.encode(by_phone_f)
-    print('='*5 + 'REQUEST' + '='*5 + '\r\n'+ str(by_phone_f)+'\r\n')
+    log.info('='*5 + 'REQUEST' + '='*5 + '\r\n'+ str(by_phone_f)+'\r\n')
     return by_phone
